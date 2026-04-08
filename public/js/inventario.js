97d5bd02b5e1efeb4fd57047db0243d53faacf87
barrak2018 <joseestebanojedaaular@gmail.com>
2026-04-08 14:42:23 -0400

    inventario

--- public/js/inventario.js
+++ public/js/inventario.js
@@ -46,7 +46,16 @@ document.addEventListener('DOMContentLoaded', async () => {
                 { field: "ID_Modelo" },
                 { field: "Estado" },
                 { field: "Observaciones" }, 
-                { field: "Modificado" },
+                { field: "Modificado" }
+            ]
+        })
+        const tablaAsignaciones = new RenderTabla("tbody-asignaciones", {
+            idField: "ID_Asignacion",
+            columns: [
+                { field: "ID_empleado" },
+                { field: "Fecha_Asignacion" },
+                { field: "Ultimo_Soporte" },
+                { field: "ID_Activo" }
             ]
         })
 
@@ -55,6 +64,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         tablaModelos.render(datos_modelos);
         const datos_activos = await inventoryAPI.getAll("activos");
         tablaActivos.render(datos_activos);
+        const datos_asignaciones = await inventoryAPI.getAll("asignaciones");
+        tablaAsignaciones.render(datos_asignaciones);
         // --- 3. Delegación de Eventos para la Tabla ---
         // Manejamos Editar y Eliminar en un solo bloque para mayor eficiencia
 
@@ -118,9 +129,47 @@ document.addEventListener('DOMContentLoaded', async () => {
                             // Refrescar datos localmente tras éxito
                             location.reload();
                             const nuevosDatos = await inventoryAPI.getAll("activos");
-                            tablaModelos.render(nuevosDatos);
+                            tablaActivos.render(nuevosDatos);
                             
                             alert("Activo eliminado correctamente.");
+                        } catch (err) {
+                            alert("Error al eliminar: " + err.message);
+                        }
+                    }
+                }
+        
+                // Lógica para el botón EDITAR
+                if (targetBtn.classList.contains('btn-edit')) {
+                    console.log("Iniciando edición del ID:", id);
+                    alert(`Funcionalidad de edición para el ID ${id} en desarrollo.`);
+                }
+            });
+        }
+        //--------------asiganciones---------------------------------
+        const tbodyAsignaciones = document.getElementById('tbody-asignaciones');
+        
+        if (tbodyAsignaciones) {
+            tbodyAsignaciones.addEventListener('click', async (e) => {
+                // Buscamos el botón más cercano al clic (por si hacen clic en el icono dentro del botón)
+                const targetBtn = e.target.closest('button');
+                if (!targetBtn) return;
+
+                const id = targetBtn.dataset.id;
+
+                // Lógica para el botón ELIMINAR
+                if (targetBtn.classList.contains('btn-delete')) {
+                    const confirmar = confirm(`¿Estás seguro de que deseas eliminar la asignacion #${id}?`);
+                    
+                    if (confirmar) {
+                        try {
+                            await inventoryAPI.delete("asignaciones", id);
+                            
+                            // Refrescar datos localmente tras éxito
+                            location.reload();
+                            const nuevosDatos = await inventoryAPI.getAll("asignaciones");
+                            tablaAsignaciones.render(nuevosDatos);
+                            
+                            alert("Asignacion eliminada correctamente.");
                         } catch (err) {
                             alert("Error al eliminar: " + err.message);
                         }
@@ -137,15 +186,22 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         // --- 4. Gestión del Formulario de Alta ---
         const formModelos = document.getElementById("form-modelos");
+        const fomrActivos = document.getElementById("form-activos");
+        const formAsignaciones = document.getElementById("form-asignaciones");
         const Fecha_produccion = formModelos.querySelector('[name="Fecha_produccion"]');
         const Fin_soporte = formModelos.querySelector('[name="Fin_soporte"]');
+
+        const fecha_Compra = fomrActivos.querySelector('[name="Fecha_compra"]');
+        const garantia = fomrActivos.querySelector('[name="Garantia"]');
         // 1. Obtenemos la fecha actual en formato YYYY-MM-DD
         const hoy = new Date().toISOString().split('T')[0];
 
         // 2. Asignamos el valor por defecto
         Fecha_produccion.value = hoy;
         Fin_soporte.value = hoy;
-
+        garantia.value = hoy;
+        fecha_Compra.value = hoy;
+        
         if (formModelos) {
             formModelos.addEventListener("submit", async (e) => {
                 e.preventDefault();
@@ -161,6 +217,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                     formModelos.reset(); // Limpiar campos
                     
                     // Recargar la tabla para mostrar el nuevo registro
+                    location.reload();
                     const nuevosDatos = await inventoryAPI.getAll("modelos");
                     tablaModelos.render(nuevosDatos);
                     
@@ -169,8 +226,104 @@ document.addEventListener('DOMContentLoaded', async () => {
                 }
             });
         }
+        //---------------activos----------------------------
+        const selccionModelo = fomrActivos.querySelector('[name="ID_Modelo"]')
+        if (datos_modelos) {
+            datos_modelos.forEach(item  => {
+                const option = document.createElement("option");
+    
+                option.value = item.ID_Modelo;
+                option.textContent = item.Modelo;
+                selccionModelo.appendChild(option);
+            });
+            
+        }
+
+        if (fomrActivos) {
+            fomrActivos.addEventListener("submit", async (e) => {
+                e.preventDefault();
+                
+                // Conversión automática de campos del formulario a objeto JSON
+                const data = Object.fromEntries(new FormData(fomrActivos));
+                console.log(data);
+                
+                
+                try {
+                    // Enviar datos a la API
+                    await inventoryAPI.create("activos", data);
+                    
+                    alert("¡Modelo guardado con éxito!");
+                    fomrActivos.reset(); // Limpiar campos
+                    
+                    // Recargar la tabla para mostrar el nuevo registro
+                    location.reload();
+                    const nuevosDatos = await inventoryAPI.getAll("activos");
+                    tablaActivos.render(nuevosDatos);
+                    
+                } catch (error) {
+                    alert("Error al guardar: " + error.message);
+                }
+            });
+        }
+        //-----------------asignaciones-------------------------
+        // 1. Seleccionamos el elemento del DOM (el select)
+        const selectActivo = formAsignaciones.querySelector('[name="ID_Activo"]');
+
+        // 2. Verificamos que el select exista Y que tengamos datos para insertar
+        if (selectActivo && datos_activos) {
+            datos_activos.forEach(item => {
+                //console.log("I'm runing");
+                let condition = item.Estado
+                if(condition.toLowerCase() == "disponible"){
+                    const option = document.createElement("option");
+
+                    option.value = item.ID_Activo;
+                    let contenido = `${item.ID_Activo}: ${item.Nombre} S/N: ${item.N_Serial}`
+                    option.textContent = contenido;
+                    selectActivo.appendChild(option);
+                }
+            });
+        }
+
+        if (formAsignaciones) {
+            formAsignaciones.addEventListener("submit", async (e) => {
+                e.preventDefault();
+                
+                // Conversión automática de campos del formulario a objeto JSON
+                const data = Object.fromEntries(new FormData(formAsignaciones));
+                console.log(data);
+                
+                
+                try {
+                    // Enviar datos a la API
+                    await inventoryAPI.create("asignaciones", data);
+                    
+                    alert("¡Asignacion guardada con éxito!");
+                    fomrActivos.reset(); // Limpiar campos
+                    
+                    // Recargar la tabla para mostrar el nuevo registro
+                    location.reload();
+                    const nuevosDatos = await inventoryAPI.getAll("asignaciones");
+                    tablaAsignaciones.render(nuevosDatos);
+                    
+                } catch (error) {
+                    alert("Error al guardar: " + error.message);
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+
 
     } catch (error) {
         console.error("Error crítico en la inicialización:", error);
     }
+
+
 });
